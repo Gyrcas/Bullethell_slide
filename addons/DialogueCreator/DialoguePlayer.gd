eff62008a7365f0048bbb43905f012429e1d94ac
DylanDucas <124808121+DylanDucas@users.noreply.github.com>
2023-10-17 16:43:28 -0400

    Typewritter added to dialogue

--- addons/DialogueCreator/DialoguePlayer.gd
+++ addons/DialogueCreator/DialoguePlayer.gd
@@ -2,18 +2,34 @@
 extends Node
 class_name DialoguePlayer
 
+##Action used to progress the dialogue
 @export var action_next : String = "ui_accept" : set = set_action_next
 
 func set_action_next(value : String) -> void:
 	action_next = value
 	update_configuration_warnings()
 
+##Parent node of button found in dialogue
 @export var button_container : Node
 
+##Node where the text will be displayed. Must contain a text variable
 @export var text_node : Node
+
+@export var use_typewritter : bool = true
+
+@export var typewritter_speed : float = 0.1
+
+##Used by script to detect when user as pressed the action key to continue dialogue
+signal confirm
+##Emitted when dialogue is finished
+signal finished
 
 enum types {msg,script,choice,condition,move_to,placeholder,choice_option}
 
+##Wether the typewritter is writing or not
+var writing : bool = false
+
+#Return dialogue if found, else null
 func get_dialogue_by_id(id : int, dialogue : Dictionary) -> Variant:
 	if dialogue.id == id:
 		return dialogue
@@ -23,21 +39,21 @@ func get_dialogue_by_id(id : int, dialogue : Dictionary) -> Variant:
 			return result
 	return null
 
-signal confirm
-
-func _input(event : InputEvent) -> void:
-	if event.is_action_pressed(action_next):
-		confirm.emit()
+##Play dialogue
 
 func play(dialogue : Dictionary, current_dialogue : Dictionary = dialogue) -> void:
 	if current_dialogue == {}:
+		finished.emit()
 		return
 	if button_container.get_child_count() > 0:
 		for child in button_container.get_children():
 			child.queue_free()
 	match int(current_dialogue.type):
 		types.msg:
-			text_node.text = current_dialogue.content
+			if use_typewritter:
+				do_typewritter(current_dialogue.content)
+			else:
+				text_node.text = current_dialogue.content
 			await confirm
 			play(dialogue,current_dialogue.children[0] if current_dialogue.children.size() > 0 else {})
 		types.script:
@@ -49,7 +65,10 @@ func play(dialogue : Dictionary, current_dialogue : Dictionary = dialogue) -> vo
 			if current_dialogue.children.size() > 0:
 				play(dialogue,current_dialogue.children[0])
 		types.choice:
-			text_node.text = current_dialogue.content
+			if use_typewritter:
+				do_typewritter(current_dialogue.content)
+			else:
+				text_node.text = current_dialogue.content
 			for child in current_dialogue.children:
 				var button : Button = Button.new()
 				button.text = child.content
@@ -63,7 +82,6 @@ func play(dialogue : Dictionary, current_dialogue : Dictionary = dialogue) -> vo
 			node.queue_free()
 			play(dialogue,current_dialogue.children[0] if result else current_dialogue.children[1])
 		types.move_to:
-			print(get_dialogue_by_id(int(current_dialogue.content),dialogue))
 			play(dialogue,get_dialogue_by_id(int(current_dialogue.content),dialogue))
 			
 
@@ -74,6 +92,37 @@ func play_from_file(path : String) -> void:
 	var file : FileAccess = FileAccess.open(path,FileAccess.READ)
 	var dialogue : Dictionary = JSON.parse_string(file.get_as_text())
 	play(dialogue)
+
+##Typewritter
+
+func do_typewritter(string : String, nb_char : int = 0) -> void:
+	writing = true
+	if timer.is_connected("timeout",on_typewritter):
+		timer.disconnect("timeout",on_typewritter)
+	timer.connect("timeout",on_typewritter.bind(string,nb_char))
+	timer.start(typewritter_speed)
+
+var timer : Timer = Timer.new()
+
+func on_typewritter(string : String, nb_char : int) -> void:
+	if !writing || nb_char > string.length():
+		writing = false
+		timer.stop()
+		text_node.text = string
+		return
+	text_node.text = string.substr(0,nb_char)
+	do_typewritter(string, nb_char + 1)
+
+func _ready() -> void:
+	if !Engine.is_editor_hint():
+		add_child(timer)
+
+func _input(event : InputEvent) -> void:
+	if event.is_action_pressed(action_next):
+		if writing:
+			writing = false
+		else:
+			confirm.emit()
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings : PackedStringArray = []
