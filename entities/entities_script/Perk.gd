62dff9846ab63c6da9d3d907c7df3e1b8683a829
DylanDucas <124808121+DylanDucas@users.noreply.github.com>
2023-11-20 18:29:53 -0500

    made boss harder

--- entities/entities_script/Perk.gd
+++ entities/entities_script/Perk.gd
@@ -4,7 +4,7 @@ class_name Perk
 @export var name : String = "unamed perk"
 @export var perk_range : float = 1200
 @export var nano_cost : float = 50
-@export var nano_reload_time : float = 3
+@export var nano_reload_time : float = 7
 
 func execute(_sender : MoverEntity,_params : Dictionary = {}) -> void:
 	push_warning("execute function not set for " + name)
