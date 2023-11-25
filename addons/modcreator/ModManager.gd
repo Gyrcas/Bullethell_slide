36cfba4b9db78bf23392784ccb4a158014d9fa0e
DylanDucas <124808121+DylanDucas@users.noreply.github.com>
2023-11-24 23:29:04 -0500

    Can now find mod folder

--- addons/modcreator/ModManager.gd
+++ addons/modcreator/ModManager.gd
@@ -1,11 +1,14 @@
 extends Node
 
+const mods_folder : String = "mods/"
+
 var config : Dictionary = {}
 
 func _ready() -> void:
-	var file : FileAccess = FileAccess.open("res://addons/ModCreator/config.json", FileAccess.READ)
-	config = JSON.parse_string(file.get_as_text())
-	var dir : DirAccess = DirAccess.open("res://"+config.mod_folder)
+	var path : String = "res://"+mods_folder
+	if !DirAccess.dir_exists_absolute(path):
+		path = OS.get_executable_path().get_base_dir()+"/"+ProjectSettings.globalize_path("res://"+mods_folder)
+	var dir : DirAccess = DirAccess.open(path)
 	if !dir:
 		return
 	config["mods"] = dir.get_directories()
